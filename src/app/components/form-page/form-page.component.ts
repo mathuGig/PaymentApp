06ea9f2861eeea0698fd1f1cd96b3a919c836b2f
thamugig <giselle.mathulada@gmail.com>
2024-06-20 07:52:29 +0700

    dialog

--- src/app/components/form-page/form-page.component.ts
+++ src/app/components/form-page/form-page.component.ts
@@ -3,9 +3,9 @@ import {
   Component,
   OnInit,
   HostBinding,
-  ViewEncapsulation,
-} from '@angular/core';
-import { CommonModule } from '@angular/common';
+  inject,
+} from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 import {
   Router,
@@ -13,7 +13,7 @@ import {
   RouterOutlet,
   ChildrenOutletContexts,
   RouterLink,
-} from '@angular/router';
+} from "@angular/router";
 
 import {
   NgForm,
@@ -24,47 +24,47 @@ import {
   FormGroupDirective,
   Validators,
   ReactiveFormsModule,
-} from '@angular/forms';
+} from "@angular/forms";
 
-import { ErrorStateMatcher } from '@angular/material/core';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatButtonModule } from '@angular/material/button';
-import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDialog, MatDialogModule } from "@angular/material/dialog";
+import { MatSelectModule } from "@angular/material/select";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatButtonModule } from "@angular/material/button";
+import { MatGridListModule } from "@angular/material/grid-list";
 import {
   MatDatepicker,
   MatDatepickerModule,
-} from '@angular/material/datepicker';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+} from "@angular/material/datepicker";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 
-import { provideNativeDateAdapter } from '@angular/material/core';
+import { provideNativeDateAdapter } from "@angular/material/core";
 
-import { provideMomentDateAdapter } from '@angular/material-moment-adapter';
+import { provideMomentDateAdapter } from "@angular/material-moment-adapter";
 
-import { DataService } from '../../service/data.service';
+import { DataService } from "../../service/data.service";
 
-import * as _moment from 'moment';
-import { default as _rollupMoment, Moment } from 'moment';
+import * as _moment from "moment";
+import { default as _rollupMoment, Moment } from "moment";
 
 const moment = _rollupMoment || _moment;
 
 export const MY_FORMATS = {
   parse: {
-    dateInput: 'MM/YY',
+    dateInput: "MM/YY",
   },
   display: {
-    dateInput: 'MM/YY',
-    monthYearLabel: 'MMM YYYY',
-    dateA11yLabel: 'LL',
-    monthYearA11yLabel: 'MMMM YYYY',
+    dateInput: "MM/YY",
+    monthYearLabel: "MMM YYYY",
+    dateA11yLabel: "LL",
+    monthYearA11yLabel: "MMMM YYYY",
   },
 };
 
 @Component({
-  selector: 'app-form-page',
+  selector: "app-form-page",
   standalone: true,
   imports: [
     CommonModule,
@@ -82,25 +82,27 @@ export const MY_FORMATS = {
     MatGridListModule,
     MatDatepickerModule,
     MatSlideToggleModule,
+    MatDialogModule,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  templateUrl: './form-page.component.html',
-  styleUrl: './form-page.component.css',
+  templateUrl: "./form-page.component.html",
+  styleUrl: "./form-page.component.css",
   providers: [provideNativeDateAdapter(), provideMomentDateAdapter(MY_FORMATS)],
 })
 export class FormPageComponent implements OnInit {
-  @HostBinding('@.disabled')
+  @HostBinding("@.disabled")
   public animationsDisabled = false;
 
   data: any;
   cardData: any;
+  readonly dialog = inject(MatDialog);
 
-  emailFormControl = new FormControl('', [
+  emailFormControl = new FormControl("", [
     Validators.required,
     Validators.email,
   ]);
 
-  isRequired = new FormControl('', [Validators.required]);
+  isRequired = new FormControl("", [Validators.required]);
 
   cardInformationForm!: FormGroup;
   submitted = false;
@@ -116,10 +118,10 @@ export class FormPageComponent implements OnInit {
   today = new Date();
   until = new Date(this.now.getFullYear() + 10, this.now.getMonth());
 
-  title = 'payment-app';
+  title = "payment-app";
 
   onSubmit() {
-    this.cardInformationForm.value.expiry = this.date.value?.format('MM/YY');
+    this.cardInformationForm.value.expiry = this.date.value?.format("MM/YY");
 
     this.submitted = true;
 
@@ -132,8 +134,13 @@ export class FormPageComponent implements OnInit {
     this.dataService
       .payment(this.cardInformationForm.value)
       .subscribe((response: any) => {
-        console.log('subscribe=>', response);
+        console.log("subscribe=>", response);
         this.gotoSuccess(response);
+        const dialogRef = this.dialog.open(DialogContentSuccessDialog);
+
+        dialogRef.afterClosed().subscribe((result) => {
+          console.log(`Dialog result: ${result}`);
+        });
       });
   }
   ngOnInit() {
@@ -145,11 +152,11 @@ export class FormPageComponent implements OnInit {
     this.cardData = this.dataService.getData();
 
     this.cardInformationForm = this.formBuilder.group({
-      cardSchemeId: ['', Validators.required],
-      cardNumber: ['', Validators.required],
-      expiry: [''],
-      name: ['', Validators.required],
-      email: ['', [Validators.email]],
+      cardSchemeId: ["", Validators.required],
+      cardNumber: ["", Validators.required],
+      expiry: [""],
+      name: ["", Validators.required],
+      email: ["", [Validators.email]],
     });
   }
   get f() {
@@ -157,19 +164,19 @@ export class FormPageComponent implements OnInit {
   }
 
   getRouteAnimationData() {
-    return this.contexts.getContext('primary')?.route?.snapshot?.data?.[
-      'animation'
+    return this.contexts.getContext("primary")?.route?.snapshot?.data?.[
+      "animation"
     ];
   }
   toggleAnimations() {
     this.animationsDisabled = !this.animationsDisabled;
   }
   gotoSuccess(response: any) {
-    console.log('gotoSuccess', response);
-    this.router.navigate(['/success'], {
+    console.log("gotoSuccess", response);
+    this.router.navigate(["/success"], {
       state: {
-        description: 'You are in needed in b',
-        Label: 'B',
+        description: "You are in needed in b",
+        Label: "B",
       },
     });
   }
@@ -186,3 +193,12 @@ export class FormPageComponent implements OnInit {
     datepicker.close();
   }
 }
+
+@Component({
+  selector: 'dialog-content-success-dialog',
+  templateUrl: './dialog-content-success-dialog.html',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DialogContentSuccessDialog {}
